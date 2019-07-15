62eebc79e6dbd53de9ea9d459e2baf8401e91512
Metin Yarci <metin.yarci@aurea.com>
2019-07-15 16:03:22 +0300

    first commit

---
+++ directory1/directory2/package1/MyFile.cs
@@ -0,0 +1,16 @@
+namespace directory1.directory2.package1
+
+public class MyFile
+{
+    public void DoWork()
+    {
+    }
+
+    public class MyInnerClass
+	{
+	    public void DoInnerWork()
+	    {
+	    }
+	}
+
+}
